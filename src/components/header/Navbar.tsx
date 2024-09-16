ce4a7bc5ce1ad372d1cb2c89e8bb7172d203e076
Tonny Kimanthi <kimaxhanthony9@gmail.com>
2024-09-16 13:45:23 +0300

    Style Navbar component

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -4,8 +4,13 @@ const Navbar = () => {
   return (
     <nav className="rounded-md bg-white/20 p-2 px-4">
       <ul className="flex items-center gap-x-8">
-        {navList.map((item) => (
-          <li className="cursor-pointer">{item}</li>
+        {navList.map((item, index) => (
+          <li
+            key={index}
+            className={`hover:text-primary cursor-pointer transition ${index === 0 ? "text-primary" : ""}`}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </nav>
